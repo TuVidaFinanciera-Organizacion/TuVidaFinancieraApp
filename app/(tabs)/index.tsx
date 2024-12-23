febdc2fa7db18732c2fed612bb1b114ba2946237
ppaillacho <ppp00031@red.ujaen.es>
2024-12-23 06:01:52 -0500

    cambio en index 2

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -16,7 +16,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Hola bienvenido!</ThemedText>
+        <ThemedText type="title">Hola bienvenido a TuVidaFinancieraApp!!</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
